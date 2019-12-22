6a346fae60390e88769c53fdb909930b9d852084
Marta K <m.kwiatkowska003@gmail.com>
2019-12-22 22:01:27 +0100

    added current date and time

---
+++ SCADA.Tomato.Plantations/Assets/Srcipts/CurrentDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentDateTime : MonoBehaviour
+{
+    public Text Value;
+    void Update()
+    {
+        Value.text = DateTimeOffset.Now.ToString();
+    }
+}
