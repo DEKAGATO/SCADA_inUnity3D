659a04d4d655764688f4ada159ffd822ba47986d
Marta K <m.kwiatkowska003@gmail.com>
2019-12-22 21:42:12 +0100

    finish working with displaying humadity values

---
+++ SCADA.Tomato.Plantations/Assets/Srcipts/HumadityProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniRx;
+
+namespace Assets.Srcipts
+{
+    public sealed class HumadityProvider
+    {
+        private BehaviorSubject<double> humadity;
+        public double Humadity => humadity.Value;
+
+        public HumadityProvider(double initValue)
+        {
+            humadity = new BehaviorSubject<double>(initValue);
+            Random random = new Random();
+
+            Observable
+                .Interval(TimeSpan.FromSeconds(1))
+                .Subscribe(lowNum =>
+                {
+                    humadity.OnNext(humadity.Value + random.Next(-1, 2) * random.NextDouble() * 0.1);
+                });
+        }
+    }
+}
