e78b7d04cdcd65f3e85013c0ce45aa92db27264f
Marta K <m.kwiatkowska003@gmail.com>
2020-01-13 13:34:34 +0100

    added camera controller with complete logic for buttons

---
+++ SCADA.Tomato.Plantations/Assets/Srcipts/CameraController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+public class CameraController : MonoBehaviour
+{
+    public Camera WholeCamera;
+    public Camera CameraA;
+    public Camera CameraB;
+    public Camera CameraC;
+
+    public Text textA;
+    public Text textB;
+    public Text textC;
+
+    private List<Text> texts;
+
+    void Start()
+    {
+        texts = new List<Text>() { textA, textB, textC };
+
+        TurnToWholeCamera();
+    }
+
+    public void TurnToWholeCamera()
+    {
+        WholeCamera.enabled = true;
+
+        CameraA.enabled = false;
+        CameraB.enabled = false;
+        CameraC.enabled = false;
+
+        texts.ForEach(x => x.enabled = true);
+    }
+
+    public void TurnToCameraA()
+    {
+        CameraA.enabled = true;
+
+        WholeCamera.enabled = false;
+        CameraB.enabled = false;
+        CameraC.enabled = false;
+
+        texts.ForEach(x => x.enabled = false);
+    }
+
+    public void TurnToCameraB()
+    {
+        CameraB.enabled = true;
+
+        WholeCamera.enabled = false;
+        CameraA.enabled = false;
+        CameraC.enabled = false;
+
+        texts.ForEach(x => x.enabled = false);
+    }
+
+    public void TurnToCameraC()
+    {
+        CameraC.enabled = true;
+
+        WholeCamera.enabled = false;
+        CameraB.enabled = false;
+        CameraA.enabled = false;
+
+        texts.ForEach(x => x.enabled = false);
+    }
+}
